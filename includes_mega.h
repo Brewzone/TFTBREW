8fa67426871e7535a03a3dbe487a6886fb8ce825
Brewzone <37047892+Brewzone@users.noreply.github.com>
2018-03-04 18:24:10 +0000

    Create includes_mega.h

---
+++ includes_mega.h
@@ -0,0 +1,19 @@
+
+//==============================================//
+// Include the various libraries                //
+//==============================================//
+
+#include <PID_v1.h>
+#include <PID_AutoTune_v0.h>
+#include <MCUFRIEND_kbv.h>
+#include <TouchScreen.h>
+#include <SPI.h>
+#include <EEPROM.h>
+#include <elapsedMillis.h>
+#include <Adafruit_MAX31865.h>
+#include <avr/wdt.h>
+#include <Adafruit_GFX.h> // Hardware-specific library
+//#include <ILI9488_kbv.h>
+//#include <TimerFive.h>
+//#include <SD.h>
+//#include <AltSoftSerial.h>
