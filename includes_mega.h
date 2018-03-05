f9499b023be974641f958b20dc2581a7809ed87b
Brewzone <37047892+Brewzone@users.noreply.github.com>
2018-03-05 13:36:41 +0000

    Update includes_mega.h

--- includes_mega.h
+++ includes_mega.h
@@ -8,7 +8,7 @@
 #include <MCUFRIEND_kbv.h> // changed this line to to 0x3A, 1, 0x55,      //Interlace Pixel Format [XX]
 // Due to incorrect colour scheme using 0x3A, 1, 0x66,      //Interlace Pixel Format [XX]
 #include <TouchScreen.h>
-#include <SPI.h>
+#include <SPI.h> // https://joshuawoehlke.com/daisy-chaining-spi-arduino-mega-2560/
 #include <EEPROM.h>
 #include <elapsedMillis.h>
 #include <Adafruit_MAX31865.h> // https://joshuawoehlke.com/daisy-chaining-spi-arduino-mega-2560/
